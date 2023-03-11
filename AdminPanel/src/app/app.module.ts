05bf46f16e80caa94ccda246b41d5f31dd212dcd
Saman Azadi <s.azadi@DESKTOP-VJHOBTS>
2023-03-11 15:12:51 +0330

    add translate

--- AdminPanel/src/app/app.module.ts
+++ AdminPanel/src/app/app.module.ts
@@ -2,7 +2,10 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -24,10 +27,21 @@ const routes: Routes = [
   ],
 
   imports: [RouterModule.forRoot(routes),
-    BrowserModule, AppRoutingModule, HttpClientModule, FormsModule
+    BrowserModule, AppRoutingModule, HttpClientModule, FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
+
   ],
   exports: [RouterModule],
   providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
