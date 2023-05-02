bb4e124ced256687289d9893478cf1eab9ff79bf
Saman Azadi <s.azadi@DESKTOP-VJHOBTS>
2023-05-02 15:25:40 +0330

    some change

--- AdminPanel/src/app/login/login.component.ts
+++ AdminPanel/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AuthenticationService } from '../core/services/authentication.service';
-import { Validators, FormBuilder } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
@@ -13,9 +13,11 @@ import { ErrorHandler } from '../core/helpers/errorHandler';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  loginForm: any;
+  loginForm: FormGroup;
 
-  constructor(private httpClient: HttpClient, public router: Router,
+  constructor(
+    private httpClient: HttpClient,
+    public router: Router,
     private titleService: Title,
     private authenticationService: AuthenticationService,
     private fb: FormBuilder,
@@ -35,7 +37,7 @@ export class LoginComponent implements OnInit {
     });
   }
   title = 'Login';
-  apiErrors: Array<String> = []
+  apiErrors: Array<String> = [];
   ngOnInit() {
     this.titleService.setTitle(this.title);
   }
@@ -43,16 +45,13 @@ export class LoginComponent implements OnInit {
     var user = this.loginForm.getRawValue();
     this.httpClient
       .post(environment.baseUrl + 'api/v1/Account/Authenticate', user)
-      .subscribe(
-        (val) => {
-          var response = Object(val);
-          this.apiErrors = this.errorHandler.GetErrors(response)
-          if (response.success) {
-            this.authenticationService.login(response);
-            this.router.navigate(['/']);
-          }
+      .subscribe((val) => {
+        var response = Object(val);
+        this.apiErrors = this.errorHandler.GetErrors(response);
+        if (response.success) {
+          this.authenticationService.login(response);
+          this.router.navigate(['/']);
         }
-      );
-
+      });
   }
 }
